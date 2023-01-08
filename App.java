2c59c0b28325b612b627caf5a6d69a38731e9b7a
Max <maxhard001@gmail.com>
2023-01-09 01:48:48 +0300

    App.java is a toy app for file functiondlity tests

---
+++ App.java
@@ -0,0 +1,64 @@
+import java.net.URI;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class App {
+    /**
+     * @throws Exception
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        
+        Path p = Paths.get("c://test//t1.txt");
+
+        System.out.println("Root: " + p.getRoot().toString());
+        System.out.println(p.getFileName().toString());
+        System.out.println(p.getParent().toString());
+        System.out.println(p.getNameCount());
+
+        URI u = URI.create("https://www.google.com.ua/images/srpr/logo11w.png");
+        Path ps = Paths.get("c://test//www123.png");
+        // Path file = Files.createFile(Paths.get("c://test//www1.png"));
+
+        System.out.println("Copying https://www.google.com.ua/images/srpr/logo11w.png");
+        try (InputStream is = u.toURL().openStream();) {
+            Files.copy(is, ps, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Copying done.");
+            System.out.println("App.main()");
+        }
+
+        Path zipFile = Paths.get("c://test//zip.zip");
+        Path fileToArc = Paths.get("c://test//t3.txt");
+
+        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(zipFile.toFile(), false));) {
+            zip.putNextEntry(new ZipEntry("file_in_arc.txt"));
+            Files.copy(fileToArc, zip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    /*     FileOutputStream zipFile = new FileOutputStream("c:/test/archive.zip");
+        ZipOutputStream zip = new ZipOutputStream(zipFile);
+
+        // кладем в него ZipEntry – «архивный объект»
+        zip.putNextEntry(new ZipEntry("document.txt"));
+
+        // копируем файл «document-for-archive.txt» в архив под именем «document.txt»
+        File file = new File("c:/test/t3.txt");
+        Files.copy(file.toPath(), zip);
+
+        // закрываем архив
+        zip.close(); */
+
+    }
+}
