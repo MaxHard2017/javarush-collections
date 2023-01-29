72d9f10dc7595e84d89e2fd67dc894a879e6215c
Max <maxhard001@gmail.com>
2023-01-29 15:07:01 +0300

    2028 - Нестандартное дерево - нчальный коммит

---
+++ com/gmail/maxhard001/javarush/collections/level1/task2028/CustomTree.java
@@ -0,0 +1,26 @@
+package com.gmail.maxhard001.javarush.collections.level1.task2028;
+
+import java.util.AbstractList;
+import java.util.Collection;
+
+public class CustomTree extends AbstractList<String>{
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends String> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+} 
---
+++ com/gmail/maxhard001/javarush/collections/level1/task2028/Solution.java
@@ -0,0 +1,5 @@
+package com.gmail.maxhard001.javarush.collections.level1.task2028;
+
+public class Solution {
+    
+}
