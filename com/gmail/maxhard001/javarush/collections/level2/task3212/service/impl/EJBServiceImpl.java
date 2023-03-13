4901e65e1b03b9003dd59649def9688c74357d59
Max IV <maxhard001@gmail.com>
2023-03-13 17:27:02 +0300

    3212 - RMI custom Servise Locator chached

---
+++ com/gmail/maxhard001/javarush/collections/level2/task3212/service/impl/EJBServiceImpl.java
@@ -0,0 +1,17 @@
+package com.gmail.maxhard001.javarush.collections.level2.task3212.service.impl;
+
+import com.gmail.maxhard001.javarush.collections.level2.task3212.service.Service;
+
+public class EJBServiceImpl implements Service {
+
+    @Override
+    public void execute() {
+        System.out.println("Executing the EJBService");
+    }
+
+    @Override
+    public String getName() {
+        return "EJBService";
+    }
+
+}
