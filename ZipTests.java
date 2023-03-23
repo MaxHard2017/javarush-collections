716e97cb397e295acf31063dfebebddab9fdf935
Max IV <maxhard001@gmail.com>
2023-03-23 18:54:40 +0300

    minor changes

--- ZipTests.java
+++ ZipTests.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
-public class App {
+public class ZipTests {
     /**
      * @throws Exception
      * @param args
